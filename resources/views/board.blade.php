d3e2cf82ef7e895dfa2c32c43e9d478ad6620a01
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-11-01 11:04:06 -0400

    finished board styling that was wrong yesterday and worked on routes for restarting a game and incrementing turns

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -3,33 +3,34 @@
 <head>
 	<title>Laravel Connect Four</title>
 
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="/style.css">
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<script src="https://use.fontawesome.com/e81701c933.js"></script>
 </head>
 <body class="container text-center">
 
 	<h1 class="mt-5 mb-4">Laravel Connect Four</h1>
 
 	<div class="row justify-content-center">
-		<div class="drop"><button class="btn btn-light ml-1 mr-1">Drop!</button></div>
-		<div class="drop"><button class="btn btn-light ml-1 mr-1">Drop!</button></div>
-		<div class="drop"><button class="btn btn-light ml-1 mr-1">Drop!</button></div>
-		<div class="drop"><button class="btn btn-light ml-1 mr-1">Drop!</button></div>
-		<div class="drop"><button class="btn btn-light ml-1 mr-1">Drop!</button></div>
-		<div class="drop"><button class="btn btn-light ml-1 mr-1">Drop!</button></div>
-		<div class="drop"><button class="btn btn-light ml-1 mr-1">Drop!</button></div>
+		<div class="drop"><button class="btn btn-light ml-1 mr-1"><i class="fa fa-arrow-down"></i></button></div>
+		<div class="drop"><button class="btn btn-light ml-1 mr-1"><i class="fa fa-arrow-down"></i></button></button></div>
+		<div class="drop"><button class="btn btn-light ml-1 mr-1"><i class="fa fa-arrow-down"></i></button></button></div>
+		<div class="drop"><button class="btn btn-light ml-1 mr-1"><i class="fa fa-arrow-down"></i></button></button></div>
+		<div class="drop"><button class="btn btn-light ml-1 mr-1"><i class="fa fa-arrow-down"></i></button></button></div>
+		<div class="drop"><button class="btn btn-light ml-1 mr-1"><i class="fa fa-arrow-down"></i></button></button></div>
+		<div class="drop"><button class="btn btn-light ml-1 mr-1"><i class="fa fa-arrow-down"></i></button></button></div>
 	</div>
 
 	<div class="mt-3 mb-3 board">
 
 		@for ($i = $rows - 1; $i >= 0; $i--)
 
-		<div class="row">
+		<div class="row justify-content-center">
 
 			@for ($j = 0; $j < $columns; $j++)
 
-				<div class="spot {{ $board[$i][$j] }}">{{ $i }}, {{ $j }}</div>
+				<div class="spot {{ $board[$i][$j] }}">{{ $i }} [{{ $j }}]</div>
 
 			@endfor
 
@@ -44,7 +45,13 @@
 	</div>
 
 	<div class="mt-3 mb-3">
-		<button class="btn btn-primary">Start New Game</button>
+		Turn: {{ $turn }}
+	</div>
+
+	<div class="mt-3 mb-3">
+		<form method="get" action="/restart">
+			<button class="btn btn-primary">Start New Game</button>
+		</form>
 	</div>
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,15 @@
 */
 
 Route::get('/', function () {
+
+	// TODO: Fix this, it's broken
 	$players = ['Red', 'Blue'];
+	$rows = 6;
+	$columns = 7;
 	$turn	= 5;
 	$currentPlayer = $players[$turn % 2];
 	$board = [];
-	$rows = 6;
-	$columns = 7;
+	// Other variables are in Game.php model
 
 	// $r < 6 because there are 6 rows and $c < 7 because there are 7 columns on the board
 	for ($r = 0; $r < $rows; $r++) {
@@ -28,5 +31,48 @@ Route::get('/', function () {
 
 	// var_dump($board);
 
-  return view('board', compact('currentPlayer', 'board', 'rows', 'columns'));
+  return view('board', compact('currentPlayer', 'board', 'rows', 'columns', 'turn'));
+
+  // Below line names the route!
+})->name('board');
+
+
+Route::get('game/{id}', function($id) {
+
+	$game = \App\Game::find($id);
+	
+	$turn = $game->turn;
+	$rows = $game->rows;
+	$columns = $game->columns;
+
+	$currentPlayer = $game->players[$turn % 2];
+	$board = [];
+
+	for ($r = 0; $r < $rows; $r++) {
+		for ($c = 0; $c < $columns; $c++) {
+			$board[$r][$c] = '';
+		}
+	}
+
+	return view('board', compact('currentPlayer', 'turn', 'board', 'rows', 'columns'));
+
+	// Can be seen by going to http://localhost:8000/game/2
+	// return "Working on game " . $id;
+
+})->name('game');
+
+
+Route::get('/restart', function() {
+
+	// TODO: End the old game (set in_progress to false?)
+
+	// Make a new game (same Tinker commands)
+	$game = new \App\Game;
+	$game->save();
+
+	$id = $game->id;
+
+	// Show the board
+	return redirect()->route('game', ['id' => $id]);
+
 });
